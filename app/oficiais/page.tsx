324dcbc87f0fc370f3d6b6c548b1356ac1a176b6
AlostXD <junio.ribeiro02@hotmail.com>
2025-03-21 01:34:29 -0300

    Fixing deployment bugs (Just commited API routes for later)

--- app/oficiais/page.tsx
+++ app/oficiais/page.tsx
@@ -3,11 +3,12 @@ import Navbar from "../../components/navbar";
 
 
 export default function Home() {
+  /*
 async function fetchOficiais() {
   const response = await fetch("/api");
   const data = await response.json();
   return Array.isArray(data) ? data : [];
-}
+}*/
 
 return (
   <>
