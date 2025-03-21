afcd070ac9c5604933c2492af495fc56c06401cd
AlostXD <junio.ribeiro02@hotmail.com>
2025-03-21 01:38:57 -0300

    Fixing deployment bugs (Just commited API routes for later) 3

--- app/oficiais/page.tsx
+++ app/oficiais/page.tsx
@@ -4,11 +4,35 @@ import Navbar from "../../components/navbar";
 
 export default function Home() {
   /*
-async function fetchOficiais() {
-  const response = await fetch("/api");
-  const data = await response.json();
-  return Array.isArray(data) ? data : [];
-}*/
+
+    (Colocar essa parte depois no route.ts em /api para funcionar)
+    import prisma from '@/app/prisma'
+
+
+    export async function GET(request: Request) {
+        const data = await prisma.oficiais.findMany();
+        const nome = data.map((oficial) => oficial.name);
+        const id = data.map((oficial) => oficial.id);
+        const gameId = data.map((oficial) => oficial.gameId);
+        const unidade = data.map((oficial) => oficial.unidade);
+        const cargo = data.map((oficial) => oficial.cargo);
+        const numero = data.map((oficial) => oficial.numero);
+
+
+        return new Response(JSON.stringify({ nome, id, gameId, unidade, cargo, numero }), {
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
+
+
+
+    async function fetchOficiais() {
+      const response = await fetch("/api");
+      const data = await response.json();
+      return Array.isArray(data) ? data : [];
+    }
+  */
 
 return (
   <>
