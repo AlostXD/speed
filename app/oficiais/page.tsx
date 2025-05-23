f255fe95fa69cf0e8a58a9de49b48bacd948d0aa
AlostXD <junio.ribeiro02@hotmail.com>
2025-05-22 22:41:23 -0300

    Alterações no package (npx prisma generate && next build)

--- app/oficiais/page.tsx
+++ app/oficiais/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Navbar from "../../components/navbar";
 import prisma from "@/app/prisma";
-import { Oficiais } from "@prisma/client";
 
 export default async function OficiaisPage() {
-  const data: Oficiais[]= await prisma.oficiais.findMany({
+  const data = await prisma.oficiais.findMany({
     where: {
     cargo: "Comando Speed"
     },
@@ -21,12 +20,10 @@ return (
           key={data.id}
           className="group relative w-80 h-[500px] overflow-hidden rounded-xl shadow-lg"
         >
-          {/* Foto do Oficial 
           <div className="absolute inset-0 bg-cover bg-center transition-transform duration-300 group-hover:translate-x-[-100%]"
             style={{ backgroundImage: `url(${data.image})` }}
-          ></div>*/}
+          ></div>
 
-          {/* Informações do Oficial */}
           <div className="absolute inset-0 bg-zinc-900 text-white p-4 flex flex-col justify-center items-center gap-4 translate-x-full transition-transform duration-300 group-hover:translate-x-0">
             <h3 className="text-xl font-bold">Nome</h3>
             <p>{data.name}</p>
