5b9779bf9cf3462a8d863d99e74c6e07a3f81118
AlostXD <junio.ribeiro02@hotmail.com>
2025-05-23 00:42:42 -0300

    Organizado a parte dos oficiais

--- app/oficiais/page.tsx
+++ app/oficiais/page.tsx
@@ -1,45 +1,14 @@
 import React from "react";
 import Navbar from "../../components/navbar";
-import prisma from "@/app/prisma";
+import OficialCard from "../../components/oficial";
+
 
 export default async function OficiaisPage() {
-  const data = await prisma.oficiais.findMany({
-    where: {
-    cargo: "Comando Speed"
-    },
-  });
 
 return (
   <>
     <Navbar />
-    <div className="flex flex-col items-center justify-center w-full m-auto gap-12 p-12">
-    <h1 className="text-4xl font-extrabold">Comandos Speed</h1>
-    <div className="flex items-center justify-center gap-12 p-12">
-      {data.map((data) => (
-        <div
-          key={data.id}
-          className="group relative w-80 h-[500px] overflow-hidden rounded-xl shadow-lg"
-        >
-          <div className="absolute inset-0 bg-cover bg-center transition-transform duration-300 group-hover:translate-x-[-100%]"
-            style={{ backgroundImage: `url(${data.image})` }}
-          ></div>
-
-          <div className="absolute inset-0 bg-zinc-900 text-white p-4 flex flex-col justify-center items-center gap-4 translate-x-full transition-transform duration-300 group-hover:translate-x-0">
-            <h3 className="text-xl font-bold">Nome</h3>
-            <p>{data.name}</p>
-            <h3 className="text-xl font-bold">Cargo</h3>
-            <p>{data.cargo}</p>
-            <h3 className="text-xl font-bold">Passaporte / Game ID</h3>
-            <p>{data.gameId}</p>
-            <h3 className="text-xl font-bold">Número</h3>
-            <p>{data.numero}</p>
-            <h3 className="text-xl font-bold">Unidade</h3>
-            <p>{data.unidade}</p>
-          </div>
-        </div>
-      ))}
-    </div>
-    </div>
+    <OficialCard cargo="Comando Speed" />
   </>
 );
 }
---
+++ components/oficial.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import prisma from "@/app/prisma";
+import Image from "next/image";
+
+type props = {
+  cargo: string;
+}
+
+export default async function OficialCard({cargo}: props) {
+  const data = await prisma.oficiais.findMany({
+    where: {
+      cargo: cargo,
+    },
+  });
+
+  console.log(data);
+
+return (
+  <>
+    <div className="flex flex-col items-center justify-center w-full m-auto gap-12 p-12">
+    <h1 className="text-4xl font-extrabold">{cargo}</h1>
+    <div className="flex items-center justify-center gap-12 p-12">
+      {data.map((data) => (
+        <div
+          key={data.id}
+          className="group relative w-80 h-[500px] overflow-hidden rounded-xl transition-all duration-300 hover:shadow-lg hover:shadow-white"
+        >
+            <div
+              className="absolute inset-0 bg-cover bg-center transition-transform duration-300"
+              style={{ backgroundImage: `url(${data.image})` }}
+            ></div>
+
+          <div className="absolute inset-0 bg-zinc-900/96 text-white p-4 flex flex-col items-start gap-4 translate-x-full transition-transform duration-300 group-hover:translate-x-0">
+            <h3 className="text-2xl font-bold">{data.name} - {data.gameId}</h3>
+            <p className="text-lg italic">{data.cargo}</p>
+            <p className="text-sm italic">{data.numero}</p>
+            <h3 className="text-xl font-bold mx-auto mt-4">Unidade</h3>
+            <p className="mx-auto">{data.unidade}</p>
+            <Image src="./logo.svg" alt="Logo" width={200} height={200} className='invert m-auto' />
+          </div>
+        </div>
+      ))}
+    </div>
+    </div>
+  </>
+);
+}
